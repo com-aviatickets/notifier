e6764f52e6875c440c9cf8e87e88777b0aa370c2
Ruslan Pavlov <rpavlov@ubic.tech>
2025-02-02 20:07:41 +0600

    initial commit

---
+++ src/test/java/com/aviatickets/notifier/NotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aviatickets.notifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
